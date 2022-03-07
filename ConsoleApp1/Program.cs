e2c5ced127627451fd81b3187571c9f9b761c7ef
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:02:59 +0800

    B10956038

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            double meal_cost = 100;
+
+            int tip_percent = 15;
+
+            int tax_perent = 8;
+
+            int x;
+
+            x = (int)meal_cost + tip_percent + tax_perent;
+
+            Console.WriteLine("總金額為:"+x);
         }
     }
 }
